bfb81f4297607260be7aba382dc2f88d73455ca9
Chris Mullen <c.mullen.2@research.gla.ac.uk>
2016-11-03 14:35:12 +0000

    For running using screen, process contains all datafiles location

---
+++ acqu_user/random14process.sh
@@ -0,0 +1,15 @@
+AcquRoot data.2016.08/Offline-CBTaggTAPS.dat --datafile /w/work14/mainz/2016-08_Recoil_Pol/CBTaggTAPS_14637.dat.xz
+AcquRoot data.2016.08/Offline-CBTaggTAPS.dat --datafile /w/work14/mainz/2016-08_Recoil_Pol/CBTaggTAPS_14844.dat.xz
+AcquRoot data.2016.08/Offline-CBTaggTAPS.dat --datafile /w/work14/mainz/2016-08_Recoil_Pol/CBTaggTAPS_14987.dat.xz
+AcquRoot data.2016.08/Offline-CBTaggTAPS.dat --datafile /w/work14/mainz/2016-08_Recoil_Pol/CBTaggTAPS_15058.dat.xz
+AcquRoot data.2016.08/Offline-CBTaggTAPS.dat --datafile /w/work14/mainz/2016-08_Recoil_Pol/CBTaggTAPS_15152.dat.xz
+AcquRoot data.2016.08/Offline-CBTaggTAPS.dat --datafile /w/work14/mainz/2016-08_Recoil_Pol/CBTaggTAPS_15207.dat.xz
+AcquRoot data.2016.08/Offline-CBTaggTAPS.dat --datafile /w/work14/mainz/2016-08_Recoil_Pol/CBTaggTAPS_15324.dat.xz
+AcquRoot data.2016.08/Offline-CBTaggTAPS.dat --datafile /w/work14/mainz/2016-08_Recoil_Pol/CBTaggTAPS_15455.dat.xz
+AcquRoot data.2016.08/Offline-CBTaggTAPS.dat --datafile /w/work14/mainz/2016-08_Recoil_Pol/CBTaggTAPS_15510.dat.xz
+AcquRoot data.2016.08/Offline-CBTaggTAPS.dat --datafile /w/work14/mainz/2016-08_Recoil_Pol/CBTaggTAPS_15633.dat.xz
+AcquRoot data.2016.08/Offline-CBTaggTAPS.dat --datafile /w/work14/mainz/2016-08_Recoil_Pol/CBTaggTAPS_15668.dat.xz
+AcquRoot data.2016.08/Offline-CBTaggTAPS.dat --datafile /w/work14/mainz/2016-08_Recoil_Pol/CBTaggTAPS_15723.dat.xz
+AcquRoot data.2016.08/Offline-CBTaggTAPS.dat --datafile /w/work14/mainz/2016-08_Recoil_Pol/CBTaggTAPS_15751.dat.xz
+AcquRoot data.2016.08/Offline-CBTaggTAPS.dat --datafile /w/work14/mainz/2016-08_Recoil_Pol/CBTaggTAPS_15808.dat.xz
+
